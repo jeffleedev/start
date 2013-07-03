0f60637d392da0e181a75e4ebe7f4aa81df7f669
jeff@remote01 <jefflee@sent.com>
2013-07-02 20:07:49 -0400

    20130702 commit

--- rfiles/test.rb
+++ rfiles/test.rb
@@ -10,8 +10,23 @@ class Song
   end
 end
 
-aSong = Song.new("Bicylops", "Flect", 260)
+class KaraokeSong < Song
+	def initialize(name, artist, duration, lyrics)
+		super(name, artist, duration)
+		@lyrics = lyrics
+	end
+
+  def to_s
+  	super + " #{@lyrics}"
+    #{}"KS: #{@name}--#{@artist} (#{@duration}), #{@lyrics}"
+  end
+end
+
+#aSong = Song.new("Bicylops", "Flect", 260)
 #print aSong.inspect
+aSong = KaraokeSong.new("My Way", "Sinatra", 225, "And now, ...")
+
+
 print aSong.to_s
 
 
